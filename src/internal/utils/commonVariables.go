cb45f7d70aa15af6feb61ca398c9a865d2f752fd
flfteym <srd18034706130@yd-code.srdcloud.cn>
2023-04-25 16:36:35 +0800

    修改配置文件

--- src/internal/utils/commonVariables.go
+++ src/internal/utils/commonVariables.go
@@ -3,14 +3,12 @@ package utils
 // Config 从redisDataMigrator.yml文件读取的配置
 type Config struct {
 	SrcRedis struct {
-		Version string   `yaml:"version"`
-		Passwd  string   `yaml:"passwd"`
-		Url     []string `yaml:"url"`
+		Passwd string   `yaml:"passwd"`
+		Url    []string `yaml:"url"`
 	} `yaml:"srcRedis"`
 
 	DestRedis struct {
-		Version string   `yaml:"version"`
-		Passwd  string   `yaml:"passwd"`
-		Url     []string `yaml:"url"`
+		Passwd string   `yaml:"passwd"`
+		Url    []string `yaml:"url"`
 	} `yaml:"destRedis"`
 }
